367e79a029b998cf5044a7a5a21fe0dcf08aa560
Anthony <abb28b@umsystem.edu>
2026-06-28 20:54:19 -0400

    found and fixed bug

--- backend/professors/models.py
+++ backend/professors/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Professor(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     department = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
     photo_url = models.URLField(blank=True)
 
     def __str__(self):
--- backend/schools/models.py
+++ backend/schools/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class College(models.Model):
     name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class College(models.Model):
 class School(models.Model):
     college = models.ForeignKey(College, on_delete=models.CASCADE, related_name='schools')
     name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self):
         return f'{self.name} ({self.college})'
